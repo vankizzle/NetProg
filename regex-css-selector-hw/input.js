fa235533ec8dacffecf937964302a646e12f516d
vankizzle <blaidzer@abv.bg>
2018-12-10 00:50:31 +0200

    asd

---
+++ regex-css-selector-hw/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z]{1}[a-z]+/,
+"exercise_2": /(088[1-9]{7})/,
+"exercise_3": /[^10]+/,
+"exercise_4": /^((?![._@$0]).)*$/,
+"exercise_5": /.*/,
+"exercise_6": /.*/
+};
+cssSelectors = {
+"exercise_1": "css",
+"exercise_2": "css",
+"exercise_3": "css",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
