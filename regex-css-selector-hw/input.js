29ed8c948efa134469faf761cc8080f9f08aea02
Ivan Draganov <ivan.draganov@microinvest.net>
2018-12-10 10:41:50 +0200

    hw-done

--- regex-css-selector-hw/input.js
+++ regex-css-selector-hw/input.js
@@ -2,17 +2,17 @@ regExps = {
 "exercise_1": /[A-Z]{1}[a-z]+/,
 "exercise_2": /(088[1-9]{7})/,
 "exercise_3": /[^10]+/,
-"exercise_4": /^((?![._@$0]).)*$/,
-"exercise_5": /.*/,
-"exercise_6": /.*/
+"exercise_4": /^[a-z][\w.]{2,15}$/,
+"exercise_5": /1[0-5]\d{1}[^1]|999/,
+"exercise_6": /class=['\\"]"*[\w ]+\\?['"]/
 };
 cssSelectors = {
-"exercise_1": "css",
-"exercise_2": "css",
-"exercise_3": "css",
-"exercise_4": "css",
-"exercise_5": "css",
-"exercise_6": "css",
-"exercise_7": "css",
-"exercise_8": "css"
+"exercise_1": "item>java",
+"exercise_2": "different>java",
+"exercise_3": ".class1 > .class1.class2",
+"exercise_4": "css>*:nth-child(3)",
+"exercise_5": "tag > java + .class1",
+"exercise_6": "item > item > item > item > item",
+"exercise_7": "#diffId2>*:nth-child(2)",
+"exercise_8": "#someId"
 };
